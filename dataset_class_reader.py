9ccc1ab54e6976faaab2fa9c744fd977577d9f26
eicchen02 <eichen02@gmail.com>
2023-06-17 23:59:15 -0400

    Updated course list to have class alternatives. Seperated webscrapping into own function

--- dataset_class_reader.py
+++ dataset_class_reader.py
@@ -9,15 +9,44 @@ class ClassReader:
             reader = csv.reader(csv_file)
             self.subject_to_code = dict(reader)
             self.class_list = []
-            self.populate_class_list()
 
-    def populate_class_list(self):
+    def scrape_html(self, major_code):
         ''' creates list of all courses that count to the concentration
         '''
-        page = urllib.request.urlopen('https://bulletin.brown.edu/the-college/concentrations/comp')
+        page = urllib.request.urlopen('https://bulletin.brown.edu/the-college/concentrations/' + major_code)
         txt = str(page.read())
-        class_code_short = set(re.findall(r'\b\w[A-Z]{3}\s\w[0-9A-Z]{3}\b', txt))
-        class_code_long = set(re.findall(r'\b\w[A-Z]{3}\s\w[0-9A-Z]{4}\b', txt))
-        self.class_list = class_code_short.union(class_code_long)
+        
+        
+        fp = open("webtext.txt", "w+")
+        fp.write(txt)
+        fp.close()
+        
+        
+    def populate_class_list(self, weblist_link):
+        
+        try:
+            fp = open(weblist_link, "r")
+            txt = fp.read()
+            fp.close()
+        except:
+            raise("Website html not found!, Try running scrape_html first")
+        
+        #~ ampersand capture might be incorrect, kinda scuffed
+        class_code = re.findall(r'\b\w[A-Z]{3}\s\w[0-9A-Z]{3,4}\b|\borclass\b|class="blockindent">&amp', txt)
+        class_code_clean = [class_code[0]]
+        
+        
+        for i in class_code:
+            if class_code_clean[-1] != i:
+                class_code_clean.append(i)
+                
+        self.class_list = class_code_clean
+        
+        print(self.class_list)
+        
+if __name__ == "__main__":
+    CR = ClassReader()
+    CR.scrape_html(CR.subject_to_code["Computer Science"])
+    CR.populate_class_list("webtext.txt")
     
 
